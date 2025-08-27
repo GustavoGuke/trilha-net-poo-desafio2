fb6cd9b4763411fa38a30c4ba98d64da1423460d
gusta <gustavo.athenas13@gmail.com>
2025-08-27 12:06:22 -0300

    resolvendo desafio implementar atributos e chamar a classe interface

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123", "Nokia", "1234", 32);
+Iphone iphone = new Iphone("456", "Iphone", "4567", 64);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
